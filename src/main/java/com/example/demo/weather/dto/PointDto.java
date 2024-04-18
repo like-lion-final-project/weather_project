ef792c9ffe70a31de6404e6d9ce1d3394a80a888
m1nddoong <alstjsdlr0321@korea.ac.kr>
2024-04-18 10:25:03 +0900

    feat: 위경도 데이터를 격자(Grid) XY 로 변환하는 기능 구현

--- src/main/java/com/example/demo/weather/dto/PointDto.java
+++ src/main/java/com/example/demo/weather/dto/PointDto.java
@@ -6,6 +6,11 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class PointDto {
+    // 위경도 좌표
     private Double lat;
     private Double lng;
+
+    // 격자 XY
+    private Integer nx;
+    private Integer ny;
 }
