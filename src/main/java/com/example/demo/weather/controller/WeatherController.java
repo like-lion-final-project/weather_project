59873eaf0976385817eaa3b9a073f02888b454d6
m1nddoong <alstjsdlr0321@korea.ac.kr>
2024-04-18 11:48:54 +0900

    remove: NCP geolocation API 코드 삭제

--- src/main/java/com/example/demo/weather/controller/WeatherController.java
+++ src/main/java/com/example/demo/weather/controller/WeatherController.java
@@ -2,12 +2,11 @@ package com.example.demo.weather.controller;
 
 import com.example.demo.weather.dto.PointDto;
 import com.example.demo.weather.dto.fcst.FcstApiResponse;
-import com.example.demo.weather.dto.geolocation.GeoLocationNcpResponse;
+
 import com.example.demo.weather.dto.ncst.NcstApiResponse;
 import com.example.demo.weather.service.VilageSrtFcstService;
 import com.example.demo.weather.service.NcpGeocodeService;
-import com.example.demo.weather.service.NcpGeolocationService;
-import java.util.Map;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class WeatherController {
     private final VilageSrtFcstService vilageSrtFcstService;
     private final NcpGeocodeService geocodeService;
-    private final NcpGeolocationService geolocationService;
 
     /**
      * 초단기 예보 조회 (시간대별 날씨)
@@ -57,20 +55,5 @@ public class WeatherController {
             String query
     ) {
         return geocodeService.getGeocode(query);
-    }
-
-    /**
-     * geolocation
-     */
-    @GetMapping("/geolocation")
-    public GeoLocationNcpResponse geoLocation(
-            @RequestParam("ip")
-            String ip
-    ) {
-        return geolocationService.geoLocation(Map.of(
-                "ip", ip,
-                "responseFormatType", "json",
-                "ext", "t"
-        ));
     }
 }
