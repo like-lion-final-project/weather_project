985ea684a88abc7f2bd499dbc5d58f2b76a33882
m1nddoong <alstjsdlr0321@korea.ac.kr>
2024-04-18 17:31:00 +0900

    feat: rgeocode 구현

--- src/main/java/com/example/demo/weather/controller/WeatherController.java
+++ src/main/java/com/example/demo/weather/controller/WeatherController.java
@@ -5,15 +5,15 @@ import com.example.demo.weather.dto.fcst.FcstApiResponse;
 
 import com.example.demo.weather.dto.ncst.NcstApiResponse;
 import com.example.demo.weather.dto.news.NDNewsResponse;
+import com.example.demo.weather.dto.rgeocoding.RGeoResponseDto;
 import com.example.demo.weather.service.GridConversionService;
-import com.example.demo.weather.service.NDApiService;
 import com.example.demo.weather.service.NDSearchService;
 import com.example.demo.weather.service.VilageSrtFcstService;
 import com.example.demo.weather.service.NcpGeocodeService;
 
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +36,17 @@ public class WeatherController {
             String query
     ) {
         return geocodeService.getGeocode(query);
+    }
+
+    /**
+     * Rgeocode
+     */
+    @GetMapping("/rgeocode")
+    public RGeoResponseDto getAddress(
+            @RequestBody
+            PointDto dto
+    ) {
+        return geocodeService.getAddress(dto);
     }
 
     /**
---
+++ src/main/java/com/example/demo/weather/dto/rgeocoding/RGeoRegionDetails.java
@@ -0,0 +1,10 @@
+package com.example.demo.weather.dto.rgeocoding;
+
+import lombok.Data;
+
+@Data
+public class RGeoRegionDetails {
+    private String name;
+    private Object coords;
+}
+
