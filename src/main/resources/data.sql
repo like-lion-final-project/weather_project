493599658f7edbb092e6b381fecfe2bd8e25b46f
m1nddoong <alstjsdlr0321@korea.ac.kr>
2024-04-24 13:48:34 +0900

    fix: application.yaml 파일, data.sql 파일 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
-# INSERT INTO assistant (instructions, is_active, name, version,model,is_delete)
-# VALUES ('You are a fashion expert who knows well about temperature and clothes. Try to recommend people the right fashion for each temperature. Please keep the answer form in JSON form as below. Please process the category in array form. Category: [Coat, T-shirt...] You have to recommend a total of six categories. Please omit the main categories such as the top and bottom. The values that enter the Input are C: Temperature, A: Age, and G: Gender. Please refer to this value.',
-#         true,'Fashion Expert_0.0.1','0.0.1','gpt-3.5-turbo',false
-#        ) ON DUPLICATE KEY UPDATE id=id;
+-- # INSERT INTO assistant (instructions, is_active, name, version,model,is_delete)
+-- # VALUES ('You are a fashion expert who knows well about temperature and clothes. Try to recommend people the right fashion for each temperature. Please keep the answer form in JSON form as below. Please process the category in array form. Category: [Coat, T-shirt...] You have to recommend a total of six categories. Please omit the main categories such as the top and bottom. The values that enter the Input are C: Temperature, A: Age, and G: Gender. Please refer to this value.',
+-- #         true,'Fashion Expert_0.0.1','0.0.1','gpt-3.5-turbo',false
+-- #        ) ON DUPLICATE KEY UPDATE id=id;
 
-INSERT INTO user (username) VALUES ('test-user')
+-- INSERT INTO user (username) VALUES ('test-user')
 
 
 
@@ -214,7 +214,7 @@ VALUES
 ('평강', '11L10003');
 
 
-INSERT INTO `cody_caster`.`mid_land_area_code`
+insert into `cody_caster`.`mid_land_area_code`
 (`area`,
  `code`)
 VALUES
