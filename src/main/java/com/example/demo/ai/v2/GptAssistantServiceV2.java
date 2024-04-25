3854fd162b856891e8a4886bd72719d96700a852
PJScript <pjs771188@gmail.com>
2024-04-25 17:02:38 +0900

    refactor: 나머지 메서드 수정완료

--- src/main/java/com/example/demo/ai/v2/GptAssistantServiceV2.java
+++ src/main/java/com/example/demo/ai/v2/GptAssistantServiceV2.java
@@ -2,6 +2,8 @@ package com.example.demo.ai.v2;
 
 import com.example.demo.ai.dto.assistant.Assistant;
 import com.example.demo.ai.dto.assistant.AssistantCreateRequest;
+import com.example.demo.ai.dto.messages.Message;
+import com.example.demo.ai.dto.messages.MessageList;
 import com.example.demo.ai.dto.run.CreateThreadAndRunRequest;
 import com.example.demo.ai.dto.run.Run;
 import com.example.demo.ai.entity.AssistantEntity;
@@ -99,24 +101,7 @@ public class GptAssistantServiceV2 {
      */
     public Run createThreadAndRun(CreateThreadAndRunRequest dto) {
 
-
-        String uri = "/v1/threads/runs";
         ResponseEntity<String> json = gptAssistantApiServiceV2.createThreadAndRun(dto);
-
-//                restClient
-//                .post()
-//                .uri(uri)
-//                .body(dto)
-//                .retrieve()
-//                .onStatus(HttpStatusCode::is4xxClientError,
-//                        (request, response) -> {
-//                            log.warn(response.toString());
-//
-//                            log.warn("에러내용: " + response.getStatusText());
-//                            log.warn("에러내용: " + response.getBody());
-//                            throw new RuntimeException("Is4xx Client Error");
-//                        })
-//                .toEntity(String.class);
 
         Long tempUser = 1L;
         User user = userRepo.findById(tempUser).orElseThrow(() -> new RuntimeException("유저가 존재하지 않습니다."));
@@ -156,5 +141,88 @@ public class GptAssistantServiceV2 {
     }
 
 
+    /**
+     * <p>메시지 단일 조회 메서드 입니다.</p>
+     * <p>조회할 때 스레드 아이디가 필요합니다. 스레드가 이미 삭제되었다면 해당 스레드에 포함되어있는 모든 메시지를 조회할 수 없습니다.</p>
+     *
+     * @param messageId 조회할 메시지의 아이디 입니다.
+     */
+    public Message getMessage(String threadId, String messageId) {
+        String uri = "/v1/threads/" + threadId + "/messages/" + messageId;
+        ResponseEntity<String> json = gptAssistantApiServiceV2.getMessage(threadId,messageId);
+//                restClient
+//                .get()
+//                .uri(uri)
+//                .retrieve()
+//                .onStatus(HttpStatusCode::is4xxClientError,
+//                        (request, response) -> {
+//                            log.warn("에러내용: " + response.getStatusText());
+//                            throw new RuntimeException("Is4xx Client Error");
+//                        })
+//                .toEntity(String.class);
+        try {
+            return objectMapper.readValue(json.getBody(), Message.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("JsonProcessingException");
+        } catch (Exception e) {
+            throw new RuntimeException("Exception");
+        }
+    }
 
+
+    /**
+     * <p>메시지 리스트 조회 메서드 입니다.</p>
+     * <p>조회할 때 스레드 아이디가 필요합니다. 스레드가 이미 삭제되었다면 해당 스레드에 포함되어있는 모든 메시지를 조회할 수 없습니다.</p>
+     *
+     * @param threadId 조회할 메시지들이 들어있는 스레드 아이디
+     */
+    public MessageList getMessages(String threadId) {
+        String uri = "/v1/threads/" + threadId + "/messages";
+        ResponseEntity<String> json = gptAssistantApiServiceV2.getMessages(threadId);
+        try {
+            return objectMapper.readValue(json.getBody(), MessageList.class);
+        } catch (JsonProcessingException e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException("JsonProcessingException");
+        } catch (Exception e) {
+            throw new RuntimeException("Exception");
+        }
+    }
+
+
+    /**
+     * <p>실행 객체를 단일 조회 하는 메서드 입니다.</p>
+     *
+     * @param threadId 실행 객체에 포함된 스레드 아이디 입니다.
+     * @param runId    실행객체의 아이디 입니다.
+     */
+    public Run getRun(String threadId, String runId) {
+        ResponseEntity<String> json = gptAssistantApiServiceV2.getRuns(threadId,runId);
+        try {
+            return objectMapper.readValue(json.getBody(), Run.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("JsonProcessingException");
+        } catch (Exception e) {
+            throw new RuntimeException("Exception");
+        }
+    }
+
+    public Optional<AssistantThreadMessage> updateMessage(String runId, String newMessageUniqueId){
+        Optional<AssistantThreadMessage> message = assistantThreadMessageRepo.findFirstByRunId(runId);
+        return message.map(assistantThreadMessage -> assistantThreadMessageRepo.save(
+                AssistantThreadMessage.builder()
+                        .id(assistantThreadMessage.getId())
+                        .messageId(newMessageUniqueId)
+                        .object(assistantThreadMessage.getObject())
+                        .assistantThread(assistantThreadMessage.getAssistantThread())
+                        .role(assistantThreadMessage.getRole())
+                        .runId(runId)
+                        .type(assistantThreadMessage.getType())
+                        .value(assistantThreadMessage.getValue())
+                        .file_ids(assistantThreadMessage.getFile_ids())
+                        .annotataions(assistantThreadMessage.getAnnotataions())
+                        .metadata(assistantThreadMessage.getMetadata())
+                        .isDeleteFromOpenAi(assistantThreadMessage.isDeleteFromOpenAi())
+                        .build()));
+    }
 }
