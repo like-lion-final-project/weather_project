210457a502b813d64b34091d8d336356ba63ec00
qotjdgns <baesunghoon81@gmail.com>
2024-05-02 11:29:59 +0900

    feat: add mypage feat
    내가 등록한 게시글, 댓글단 게시글, 최근에 본 게시글 조회 기능 추가

--- src/main/java/com/example/demo/community/service/CommentService.java
+++ src/main/java/com/example/demo/community/service/CommentService.java
@@ -1,13 +1,34 @@
 package com.example.demo.community.service;
 
+import com.example.demo.community.dto.CommentDto;
 import com.example.demo.community.repository.CommentRepository;
+import com.example.demo.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class CommentService {
     private final CommentRepository commentRepository;
+
+    public List<CommentDto> readAllMyComment(User user, Integer offset) {
+        Pageable page = PageRequest.of(offset, 4);
+
+        return commentRepository.findAllByUserEntity(user, page)
+                .stream()
+                .map(CommentDto::fromEntity)
+                .collect(Collectors.toList());
+    }
+
+    public Integer getMyCommentListSize(User user) {
+        Integer listSize = commentRepository.findAllByUserEntity(user).size();
+        return listSize == 0 ? 1 : listSize;
+    }
 }
